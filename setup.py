461875b48c9ab97a83016762ad0ac380738a11e7
unknown <sribalajih@gmail.com>
2021-11-02 21:47:19 +0530

    setup complete

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+PROJECT_NAME = "oneNeuron_pypi"
+USER_NAME = "balaji04yuvaraj"
+
+setuptools.setup(
+    name=f"{PROJECT_NAME}-{USER_NAME}",
+    version="0.0.1",
+    author=USER_NAME,
+    author_email="sribalajih@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{USER_NAME}}/{PROJECT_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/balaji04yuvaraj/oneNeuron/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+    install_requires=["numpy"
+                     ,"tqdm"]
+)
